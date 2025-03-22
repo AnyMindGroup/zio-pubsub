af446ffb268711c8d7de0b67e0179c967135f227
Roman Langolf <rolang@pm.me>
2025-03-22 10:59:12 +0700

    cleanup on merge conflict resolution

--- zio-pubsub/shared/src/main/scala/com/anymindgroup/pubsub/model/OrderingKey.scala
+++
@@ -1,7 +0,0 @@
-package com.anymindgroup.pubsub
-
-opaque type OrderingKey = String
-object OrderingKey:
-  def fromString(o: String): Option[OrderingKey] = Option(o).filter(_.nonEmpty)
-
-  extension (x: OrderingKey) def value: String = x
