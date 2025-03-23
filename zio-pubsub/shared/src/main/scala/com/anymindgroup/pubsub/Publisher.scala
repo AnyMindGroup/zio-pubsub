87719af035d5c831f1705f7ff8e8b7747ecaf2f5
Roman Langolf <rolang@pm.me>
2025-03-23 12:01:06 +0700

    remove deprecated accessor methods

--- zio-pubsub/shared/src/main/scala/com/anymindgroup/pubsub/Publisher.scala
+++ zio-pubsub/shared/src/main/scala/com/anymindgroup/pubsub/Publisher.scala
@@ -1,14 +1,9 @@
 package com.anymindgroup.pubsub
 
-import zio.{NonEmptyChunk, RIO, Tag, ZIO}
+import zio.{NonEmptyChunk, RIO}
 
 trait Publisher[R, E] {
   def publish(message: PublishMessage[E]): RIO[R, MessageId]
 
   def publish(message: NonEmptyChunk[PublishMessage[E]]): RIO[R, NonEmptyChunk[MessageId]]
-}
-
-object Publisher {
-  def publish[R, E](message: PublishMessage[E])(implicit t: Tag[Publisher[R, E]]): RIO[Publisher[R, E] & R, MessageId] =
-    ZIO.serviceWithZIO[Publisher[R, E]](_.publish(message))
 }
--- zio-pubsub/shared/src/main/scala/com/anymindgroup/pubsub/Subscriber.scala
+++ zio-pubsub/shared/src/main/scala/com/anymindgroup/pubsub/Subscriber.scala
@@ -11,15 +11,3 @@ trait Subscriber {
   ): ZStream[R, Throwable, Receipt[E]] =
     subscribeRaw(subscriptionName).via(Pipeline.deserializerPipeline(deserializer))
 }
-
-object Subscriber {
-  def subscribeRaw(subscriptionName: SubscriptionName): ZStream[Subscriber, Throwable, RawReceipt] =
-    ZStream.serviceWithStream[Subscriber](_.subscribeRaw(subscriptionName))
-
-  def subscribe[R, E](
-    subscriptionName: SubscriptionName,
-    deserializer: Deserializer[R, E],
-  ): ZStream[R & Subscriber, Throwable, Receipt[E]] =
-    ZStream.serviceWithStream[Subscriber](_.subscribe(subscriptionName, deserializer))
-
-}
