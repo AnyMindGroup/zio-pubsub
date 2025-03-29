19a73a2bf6a1fc689b28f9e86d50692653f35d6c
Roman Langolf <rolang@pm.me>
2025-03-29 15:03:53 +0700

    add http subscriber specs

---
+++ zio-pubsub-test/shared/src/test/scala/com/anymindgroup/pubsub/http/HttpSubsrciberSpec.scala
@@ -0,0 +1,75 @@
+package com.anymindgroup.pubsub
+package http
+
+import com.anymindgroup.gcp.auth.{AuthedBackend, TokenProvider}
+import sttp.client4.impl.zio.RIOMonadAsyncError
+import sttp.client4.testing.{BackendStub, ResponseStub}
+import sttp.model.StatusCode
+
+import zio.test.*
+import zio.test.Assertion.*
+import zio.{Ref, Schedule, Task, ZIO}
+
+object HttpSubscriberSpec extends ZIOSpecDefault {
+  override def spec: Spec[Any, Any] = suite("HttpSubscriberSpec")(
+    List(true, false).map: ack =>
+      test(s"failed ${if ack then "ack" else "nack"} requests are retried by provided schedule") {
+        ZIO.scoped:
+          for {
+            ackCounter <- Ref.make(0)
+            backend    <- backendStub(ackCounter)
+            maxRetries  = 5
+            subscriber <- makeSubscriber(
+                            backend = Some(backend),
+                            retrySchedule = Schedule.recurs(maxRetries),
+                          )
+            exit <- subscriber
+                      .subscribe(SubscriptionName("any", "any"), Serde.utf8String)
+                      .mapZIO((_, reply) => if ack then reply.ack() else reply.nack())
+                      .runDrain
+                      .exit
+            _ <- assertTrue(exit.isFailure)
+            _ <- assertZIO(ackCounter.get)(equalTo(maxRetries))
+          } yield assertCompletes
+      }
+  )
+
+  private def backendStub(ackCounter: Ref[Int]) =
+    Ref
+      .make(0)
+      .map: pullCount =>
+        AuthedBackend(
+          TokenProvider.noTokenProvider,
+          BackendStub[Task](new RIOMonadAsyncError[Any])
+            .whenRequestMatches(r => r.uri.pathToString.endsWith(":pull"))
+            .thenRespondF {
+              // return a message on first pull only
+              pullCount.get.flatMap:
+                case 0 =>
+                  pullCount.getAndIncrement.as(
+                    ResponseStub.adjust(
+                      """|{
+                         |  "receivedMessages":[
+                         |    {
+                         |      "ackId": "1",
+                         |      "message": {
+                         |        "data": "",
+                         |        "messageId": "1",
+                         |        "publishTime": "2025-01-01T00:00:00Z"
+                         |      }
+                         |    }
+                         |  ]
+                         |}""".stripMargin,
+                      StatusCode.Ok,
+                    )
+                  )
+                case _ => ZIO.never
+            }
+            .whenRequestMatches(r => r.uri.pathToString.endsWith(":acknowledge"))
+            // return failure on any acknowledge request
+            .thenRespondF(ackCounter.getAndIncrement.as(ResponseStub.adjust("", StatusCode(400))))
+            .whenAnyRequest
+            .thenRespondNotFound(),
+        )
+
+}
